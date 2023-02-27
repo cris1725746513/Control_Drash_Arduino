762846491f1d37ae77823c98597369222e545996
CristianCastilloLeones <cristian.castillo2018@uteq.edu.ec>
2023-02-27 12:54:56 -0500

    config

--- src/app/component/graficas/graficas.component.ts
+++ src/app/component/graficas/graficas.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit ,Input } from '@angular/core';
 import { ChartConfiguration } from 'chart.js';
 
 
@@ -9,16 +9,13 @@ import { ChartConfiguration } from 'chart.js';
 })
 export class GraficasComponent implements OnInit {
 
+  @Input() horas!: any[];
+  @Input() niveles!: any[];
+
   public barChartLegend = true;
   public barChartPlugins = [];
 
-  public barChartData: ChartConfiguration<'bar'>['data'] = {
-    labels: [ '2006', '2007', '2008', '2009', '2010', '2011', '2012' ],
-    datasets: [
-      { data: [ 65, 59, 80, 81, 56, 55, 40 ], label: 'Series A' },
-      { data: [ 28, 48, 40, 19, 86, 27, 90 ], label: 'Series B' }
-    ]
-  };
+  public barChartData!: ChartConfiguration<'bar'>['data'];
 
   public barChartOptions: ChartConfiguration<'bar'>['options'] = {
     responsive: false,
@@ -26,9 +23,18 @@ export class GraficasComponent implements OnInit {
 
   constructor() {
 
+
   }
 
   ngOnInit() {
+    this.barChartData= {
+      labels:  this.horas,
+      datasets: [
+        { data: this.niveles , label: 'Control de nivel por Horas' },
+
+      ]
+    };
+
   }
 
 
--- src/app/models/casilleros.model.ts
+++
@@ -1,7 +0,0 @@
-import {Pasillos} from './pasillos.model'
-export class Casilleros{
-    idcasillero!:number;
-    estado!:boolean;
-    casillero!:string;
-    idpasillo!:Array<Pasillos>;
-}
